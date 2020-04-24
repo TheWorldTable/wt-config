658121eb1528bcac456af49e4e950a5c5a74bdb8
Mike Scalora <mike@scalora.org>
2020-04-23 18:58:32 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyB2FRhWjFGCvUWtFL8cmi9hR8XVJdc455A',
+    authDomain: 'twt-dev-ms.firebaseapp.com',
+    databaseURL: 'https://twt-dev-ms.firebaseio.com',
+    projectId: 'twt-dev-ms',
+    storageBucket: 'twt-dev-ms.appspot.com',
+    messagingSenderId: '469365682883'
+  }
+}
